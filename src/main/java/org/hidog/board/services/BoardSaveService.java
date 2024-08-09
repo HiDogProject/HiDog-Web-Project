36fa87eb339f177a9671928dec843a7be345690b
parksaehyun <psh666777@naver.com>
2024-08-09 18:05:24 +0900

    feat: 보드 작성 기초 뷰 구성완료, 공통 처리(css, js) 분리작업중

--- src/main/java/org/hidog/board/services/BoardSaveService.java
+++ src/main/java/org/hidog/board/services/BoardSaveService.java
@@ -1,4 +1,11 @@
 package org.hidog.board.services;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
 public class BoardSaveService {
 }
