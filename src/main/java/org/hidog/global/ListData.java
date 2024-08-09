4e46fb3b1742712ff855b22ea6ae39e4ebe4c244
jeongsuri <jeongsuri96@gmail.com>
2024-08-09 14:01:15 +0900

    feat: 페이징 기능 구현
    페이징 관련 기능 구현 중

---
+++ src/main/java/org/hidog/global/ListData.java
@@ -0,0 +1,13 @@
+package org.hidog.global;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class ListData<T> {
+
+    private List<T> items; //목록 데이터
+    private Pagination pagination;
+
+}
