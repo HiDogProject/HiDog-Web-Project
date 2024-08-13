adedba560bd7462e486e728fcc1f28486cfd5cc0
KIMJEONGHYEON1016 <dkemfdk4@naver.com>
2024-08-13 10:30:37 +0900

    BoardData

--- src/main/java/org/hidog/board/entities/BoardData.java
+++ src/main/java/org/hidog/board/entities/BoardData.java
@@ -13,10 +13,10 @@ import org.hidog.member.entities.Member;
 @Builder
 @NoArgsConstructor @AllArgsConstructor
 public class BoardData extends BaseEntity {
-
     @Id @GeneratedValue
     private Long seq;
 
+    @Column(length=65, nullable = false)
     private String gid;
 
     @JoinColumn(name="bid")
@@ -26,17 +26,49 @@ public class BoardData extends BaseEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     private Member member;
 
-    private String guestPw; // 비화원 비밀번호(수정, 삭제)
+    @Column(length=65)
+    private String guestPw; // 비회원 비밀번호(수정, 삭제)
 
-    private String category; //게시글 분류
+    @Column(length=60)
+    private String category; // 게시글 분류
 
-    private String poster; //작성자
+    private boolean notice; // 공지글 여부
 
-    private String subject; //제목
+    @Column(length=40, nullable = false)
+    private String poster;
 
-    private String content; //내용
+    @Column(nullable = false)
+    private String subject;
 
-    private int viewCount; //조회수
+    @Lob
+    @Column(nullable = false)
+    private String content;
+
+    private int viewCount; // 조회수
     private boolean editorView; // 에디터를 사용해서 글 작성했는지 여부
+
+    @Column(length=20)
+    private String ip; // IP 주소
+
+    private String ua; // User-Agent
+
+    private Long num1; // 정수 추가 필드1
+    private Long num2; // 정수 추가 필드2
+    private Long num3; // 정수 추가 필드3
+
+    @Column(length=100)
+    private String text1; // 한줄 텍스트 추가 필드1
+
+    @Column(length=100)
+    private String text2; // 한줄 텍스트 추가 필드2
+
+    @Column(length=100)
+    private String text3; // 한줄 텍스트 추가 필드3
+
+    @Lob
+    private String longText1; // 여러줄 텍스트 추가 필드1
+
+    @Lob
+    private String longText2; // 여러줄 텍스트 추가 필드2
 
 }
