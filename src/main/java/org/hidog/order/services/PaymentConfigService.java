8fa535ba774c2a254a59df7ff061abcc81373561
parksaehyun <psh666777@naver.com>
2024-08-13 16:36:06 +0900

    feat: 결제API 결제요청 뷰 연동중

--- src/main/java/org/hidog/order/services/PaymentConfigService.java
+++ src/main/java/org/hidog/order/services/PaymentConfigService.java
@@ -5,7 +5,6 @@ import org.hidog.config.services.ConfigInfoService;
 import org.hidog.global.SHA256;
 import org.springframework.stereotype.Service;
 
-import java.security.NoSuchAlgorithmException;
 import java.util.Date;
 
 @Service
@@ -13,37 +12,38 @@ import java.util.Date;
 public class PaymentConfigService {
     private final ConfigInfoService infoService;
 
-    public PaymentConfig get(long oid, int price) { // oid : 주문번호, price : 가격
-        PaymentConfig config = infoService.get("payment", PaymentConfig.class)
-                .orElseGet(PaymentConfig::new);
-
-        long timestamp = new Date().getTime();
-        String signKey = config.getSignKey();
-
-        // 필수로 필요한 데이터 2차가공
+    public PaymentConfig get(long oid, int price) {
         try {
+            PaymentConfig config = infoService.get("payment", PaymentConfig.class)
+                    .orElseGet(PaymentConfig::new);
+
+            long timestamp = new Date().getTime();
+            String signKey = config.getSignKey();
+
+
             // signature S
             String data = String.format("oid=%d&price=%d&timestamp=%d", oid, price, timestamp);
             String signature = SHA256.encrypt(data);
             config.setSignature(signature);
-            //signature E
+            // signature E
 
             // verification S
-            data=String.format("oid=%d&price=%d&signKey=%s&timestamp=%d", oid, price, signature, timestamp);
-            String verification = SHA256.encrypt(data); // 해시만들기
+            data = String.format("oid=%d&price=%d&signKey=%s&timestamp=%d", oid, price, signKey, timestamp);
+            String verification = SHA256.encrypt(data);
             config.setVerification(verification);
             // verification E
 
-            // mKey S
+            // mKey
             String mKey = SHA256.encrypt(signKey);
             config.setMKey(mKey);
-            // mKey E
 
-        }catch(NoSuchAlgorithmException e) {
+            config.setTimestamp(timestamp);
+            return config;
+
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
-        config.setTimestamp(timestamp);
-        return config;
+        return null;
     }
 }
