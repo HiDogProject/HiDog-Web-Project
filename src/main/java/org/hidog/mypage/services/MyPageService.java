28acea64655ab260dbf0efc149d9dfffb953be23
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-08-13 17:55:29 +0900

    MyPageService 수정 중

--- src/main/java/org/hidog/mypage/services/MyPageService.java
+++ src/main/java/org/hidog/mypage/services/MyPageService.java
@@ -1,17 +1,22 @@
 package org.hidog.mypage.services;
 
 import lombok.RequiredArgsConstructor;
-import org.hidog.member.services.MemberSaveService;
-import org.hidog.mypage.entities.RequestProfile;
+import org.hidog.member.entities.Member;
+import org.hidog.member.exceptions.MemberNotFoundException;
+import org.hidog.member.repositories.MemberRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class MyPageService {
 
-    private final MemberSaveService saveService;
+    private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public void update(RequestProfile form) {
-
+    // 회원 정보 수정 시 본인인증 -> 비밀번호
+    public boolean checkPassword(String email, String rawPassword) {
+        Member member = memberRepository.findByEmail(email).orElseThrow(MemberNotFoundException::new);
+        return passwordEncoder.matches(rawPassword, member.getPassword());
     }
 }
