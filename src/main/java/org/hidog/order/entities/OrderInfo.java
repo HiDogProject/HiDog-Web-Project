9115a18d489461dbe1066ce11ff1d929e4b06573
jeongsuri <jeongsuri96@gmail.com>
2024-08-19 18:06:44 +0900

    feat : 주문서, 주문상품과 결제 시스템 연동
    주문서, 주문상품과 결제 시스템 연동

--- src/main/java/org/hidog/order/entities/OrderInfo.java
+++ src/main/java/org/hidog/order/entities/OrderInfo.java
@@ -7,9 +7,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hidog.global.entities.BaseEntity;
 import org.hidog.member.entities.Member;
+import org.hidog.order.constants.OrderStatus;
 import org.hidog.payment.constants.PayMethod;
-
-import java.util.List;
 
 @Data
 @Entity
@@ -21,8 +20,12 @@ public class OrderInfo extends BaseEntity {
     @Id
     private Long orderNo = System.currentTimeMillis();
 
-    @OneToMany(mappedBy ="orderInfo", fetch = FetchType.LAZY)
-    private List<OrderItem> orderItems;
+    @Column(length = 20)
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status = OrderStatus.START;
+
+    @OneToOne(mappedBy ="orderInfo", fetch = FetchType.LAZY)
+    private OrderItem orderItem;
 
     @ManyToOne(fetch = FetchType.LAZY)
     private Member member;
@@ -49,6 +52,11 @@ public class OrderInfo extends BaseEntity {
 
     private String deliveryMemo; //나머지 배송지 주소
 
+    @Column(length = 60)
+    private String deliveryCompany; //배송업체
+    @Column(length = 60)
+    private String deliveryInvoice; //운송장번호
+
     @Column(length = 20)
     @Enumerated(EnumType.STRING)
     private PayMethod payMethod;
@@ -58,5 +66,15 @@ public class OrderInfo extends BaseEntity {
 
     @Column(length = 65)
     private String payTid; //PG 거래 ID(tid)
+
+    @Column(length = 40)
+    private String payBankName; //가상 계좌 은행
+    @Column(length = 40)
+    private String payBankAccount; //가상 계좌
+
+
+
+    @Transient
+    private int totalPayPrice; //총결제금액
 
 }
--- src/main/java/org/hidog/order/entities/OrderItem.java
+++ src/main/java/org/hidog/order/entities/OrderItem.java
@@ -4,7 +4,6 @@ import jakarta.persistence.*;
 import lombok.*;
 import org.hidog.board.entities.BoardData;
 import org.hidog.global.entities.BaseEntity;
-import org.hidog.order.constants.OrderStatus;
 
 @Data
 @Entity
@@ -16,9 +15,6 @@ public class OrderItem extends BaseEntity {
     @Id @GeneratedValue
     private Long seq;
 
-    @Column(length = 20)
-    @Enumerated(EnumType.STRING)
-    private OrderStatus status = OrderStatus.START;
 
     private String itemName; //ㅇㅇ님의 구매, ㅇㅇ님의 예약 활용가능
 
@@ -26,13 +22,11 @@ public class OrderItem extends BaseEntity {
     private BoardData boardData;
 
     @ToString.Exclude
-    @ManyToOne(fetch = FetchType.LAZY)
+    @OneToOne(fetch = FetchType.LAZY)
     private OrderInfo orderInfo;
 
     private int price; //상품 단품 금액
 
     private int qty; //주문 수량
-
-
 
 }
