f0cc7fd09741c2ceb0db1673791e32e422f0aac8
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-08-19 16:16:31 +0900

    myhome.js : 마이 페이지 js

--- src/main/resources/static/front/js/mypage/myhome.js
+++ src/main/resources/static/front/js/mypage/myhome.js
@@ -1,39 +1,28 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const editProfileBtn = document.getElementById('editProfileBtn');
-    const profilePopup = document.getElementById('profilePopup');
-    const cancelBtn = document.getElementById('cancelBtn');
-    const uploadForm = document.getElementById('uploadForm');
+function openProfilePopup() { // 팝업 열기
+    document.getElementById('profilePopup').style.display = 'block';
+}
 
-    // 프로필 수정 버튼 클릭 시 팝업 열기
-    editProfileBtn.addEventListener('click', function() {
-        profilePopup.style.display = 'block';
-    });
+function closeProfilePopup() { // 팝업 닫기
+    document.getElementById('profilePopup').style.display = 'none';
+}
 
-    // 취소 버튼 클릭 시 팝업 닫기
-    cancelBtn.addEventListener('click', function() {
-        profilePopup.style.display = 'none';
-    });
+document.getElementById('uploadForm').addEventListener('submit', function(event) { // 폼 제출 처리
+    event.preventDefault(); // 기본 폼 제출 방지
 
-    // 폼 제출 시 팝업 닫기 및 프로필 이미지 업데이트
-    uploadForm.addEventListener('submit', function(event) {
-        event.preventDefault(); // 기본 폼 제출 동작 방지
+    var formData = new FormData(this);
 
-        const formData = new FormData(uploadForm);
-
-        fetch(uploadForm.action, {
-            method: 'POST',
-            body: formData
+    fetch('/mypage/myhome', {
+        method: 'POST',
+        body: formData
+    })
+        .then(response => {
+            if (response.redirected) {
+                window.location.href = response.url;
+            } else {
+                console.error('Error:', response);
+            }
         })
-            .then(response => response.json())
-            .then(data => {
-                if (data.success) {
-                    document.getElementById('profileImage').src = data.imageUrl; // 이미지 URL 업데이트
-                    alert(data.successMessage);
-                    profilePopup.style.display = 'none'; // 팝업 닫기
-                } else {
-                    alert(data.errorMessage);
-                }
-            })
-            .catch(error => console.error('Error:', error));
-    });
+        .catch(error => {
+            console.error('Error:', error);
+        });
 });
