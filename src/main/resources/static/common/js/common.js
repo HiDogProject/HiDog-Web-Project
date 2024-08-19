c9c743196cefc38a617efa5e27a5d389d308ef11
parksaehyun <psh666777@naver.com>
2024-08-19 14:07:31 +0900

    feat: 파일업로드 구현중, 에디터 기능 구현중

--- src/main/resources/static/common/js/common.js
+++ src/main/resources/static/common/js/common.js
@@ -11,13 +11,15 @@ const commonLib = {
 
         const csrfToken = document.querySelector("meta[name='csrf_token']")?.content?.trim();
         const csrfHeader = document.querySelector("meta[name='csrf_header']")?.content?.trim();
-
+        console.log("url1", url);
         if (!/^http[s]?/i.test(url)) {
             let rootUrl = document.querySelector("meta[name='rootUrl']")?.content?.trim() ?? '';
             rootUrl = rootUrl === '/' ? '' : rootUrl;
 
-            url = location.protocol + "//" + location.host + rootUrl + url;
+            url = /^http[s]?/i.test(rootUrl) ? rootUrl + url : location.protocol + "//" + location.host + rootUrl + url;
+            console.log(/^http[s]?/i.test(rootUrl), rootUrl, "url2", url);
         }
+
         method = method.toUpperCase();
         if (method === 'GET') {
             data = null;
