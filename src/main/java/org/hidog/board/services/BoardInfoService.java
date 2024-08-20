6dfef0d552c73ee100571f41077a4dc428b4a12c
parksaehyun <psh666777@naver.com>
2024-08-20 12:50:55 +0900

    chore : 게시글 수정 시 게시글엔티티 -> 커맨드객체 modelMapper로 변환 오류 수정 중
    수정은 가능하나 필드에러가 뜨지 않는 오류 발생
    
    모델매퍼 컨버터 말고의 방식 구상중

--- src/main/java/org/hidog/board/services/BoardInfoService.java
+++ src/main/java/org/hidog/board/services/BoardInfoService.java
@@ -25,7 +25,9 @@ import org.hidog.global.Utils;
 import org.hidog.global.constants.DeleteStatus;
 import org.hidog.member.MemberUtil;
 import org.hidog.member.entities.Member;
+import org.modelmapper.Converter;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.spi.MappingContext;
 import org.springframework.data.web.OffsetScrollPositionHandlerMethodArgumentResolver;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -273,9 +275,9 @@ public class BoardInfoService {
 
     public RequestBoard getForm(BoardData item, DeleteStatus status) {
         // ModelMapper 인스턴스 생성
-
+        ModelMapper modelMapper = new ModelMapper();
         // 사용자 정의 Converter 추가
-        /*
+
         modelMapper.addConverter(new Converter<String, Boolean>() {
             @Override
             public Boolean convert(MappingContext<String, Boolean> context) {
@@ -293,7 +295,7 @@ public class BoardInfoService {
                     mapper.map(BoardData::isNotice, RequestBoard::setNotice); // 명시적 매핑
                     // 필요한 다른 필드도 추가로 매핑 설정 가능
                   });
-        */
+
         // 매핑 실행
         RequestBoard form = modelMapper.map(item, RequestBoard.class);
 
