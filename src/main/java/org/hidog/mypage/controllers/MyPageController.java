2c442e3f0b5794adf693618c52a33412a4b2c349
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-08-22 18:31:45 +0900

    MyPageController : 강사님 수정 1차 (8/22)

--- src/main/java/org/hidog/mypage/controllers/MyPageController.java
+++ src/main/java/org/hidog/mypage/controllers/MyPageController.java
@@ -4,6 +4,8 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.hidog.global.Utils;
 import org.hidog.global.exceptions.ExceptionProcessor;
+import org.hidog.member.services.MemberSaveService;
+import org.hidog.mypage.validators.ProfileUpdateValidator;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,8 +18,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequiredArgsConstructor
 public class MyPageController implements ExceptionProcessor {
 
+    private final ProfileUpdateValidator profileUpdateValidator;
+    private final MemberSaveService saveService;
     private final Utils utils;
 
+    // 마이 페이지 홈
     @GetMapping("/myhome")
     public String myHome() {
 
@@ -30,9 +35,18 @@ public class MyPageController implements ExceptionProcessor {
         return utils.tpl("mypage/info");
     }
 
+    // 회원 정보 수정
     @PostMapping("/info")
     public String infoSave(@Valid RequestProfile form, Errors errors) {
 
-        return utils.tpl("mypage/info");
+        profileUpdateValidator.validate(form, errors);
+
+        if (errors.hasErrors()) {
+            return utils.tpl("mypage/info");
+        }
+
+        saveService.save(form);
+
+        return "redirect:" + utils.redirectUrl("mypage/myhome");
     }
 }
