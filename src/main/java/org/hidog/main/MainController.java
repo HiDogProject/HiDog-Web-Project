f295fdfbb87304f439c774fe115658b65424eb09
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-08-26 14:45:15 +0900

    MainController : 메인 페이지 컨트롤러 초기 구성

--- src/main/java/org/hidog/main/MainController.java
+++ src/main/java/org/hidog/main/MainController.java
@@ -16,20 +16,31 @@ public class MainController {
 
     private final BoardConfigInfoService configInfoService;
 
-    @GetMapping("/main/index2")
-    public String boardList(Model model) {
+    @GetMapping
+    public String Main(Model model) {
 
-         List<String[]> boardList = configInfoService.getBoardList();
-
-        model.addAttribute("boardList", boardList);
+        List<String[]> boardList = configInfoService.getBoardList();
 
         model.addAttribute("addCss", List.of("main/style"));
         model.addAttribute("addScript", List.of( "main/dropdown"));
 
-        model.addAttribute("addCommonCss", List.of("swiper-bundle.min"));
-        model.addAttribute("addScript", List.of( "main/banner"));
-        model.addAttribute("addCommonScript", List.of("swiper-bundle.min"));
-
-        return "front/main/index2";
+        return "front/main/index";
     }
+
+//    @GetMapping("/main/index2")
+//    public String boardList(Model model) {
+//
+//         List<String[]> boardList = configInfoService.getBoardList();
+//
+//        model.addAttribute("boardList", boardList);
+//
+//        model.addAttribute("addCss", List.of("main/style"));
+//        model.addAttribute("addScript", List.of( "main/dropdown"));
+//
+//        model.addAttribute("addCommonCss", List.of("swiper-bundle.min"));
+//        model.addAttribute("addScript", List.of( "main/banner"));
+//        model.addAttribute("addCommonScript", List.of("swiper-bundle.min"));
+//
+//        return "front/main/index2";
+//    }
 }
