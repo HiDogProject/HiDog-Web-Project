611f40e2814dafce9186c4ebbde8d4286c212718
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-08-26 20:41:31 +0900

    info.js : 회원 정보 수정 페이지 js

--- src/main/resources/static/front/js/mypage/info.js
+++ src/main/resources/static/front/js/mypage/info.js
@@ -1,3 +1,11 @@
 function fileUploadCallback(files) {
-    console.log(files);
+    if (files.length === 0) return;
+
+    const el = document.querySelector(".profile-image");
+    if (!el) {
+        return;
+    }
+
+    el.style.backgroundImage = "url('" + files[0].fileUrl + "') no-repeat center center;";
+    // el.style.backgroundSize = "cover";
 }
