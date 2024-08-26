24fcf2ec987ba7039e8a51481c8a089fbeaf7fda
jeongsuri <jeongsuri96@gmail.com>
2024-08-26 11:04:50 +0900

    feat : 게시글 리스트에서 게시글 작성시간 utils 추가

---
+++ src/main/java/org/hidog/order/validators/OrderValidator.java
@@ -0,0 +1,21 @@
+package org.hidog.order.validators;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+@Component
+@RequiredArgsConstructor
+public class OrderValidator implements Validator {
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return false;
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+
+    }
+}
