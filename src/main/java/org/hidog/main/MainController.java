94f10ac01930d2b68f02fc2a32601b7b105b40a0
jeongsuri <jeongsuri96@gmail.com>
2024-08-26 11:18:26 +0900

    feat : 게시글 리스트에서 게시글 작성시간 utils 추가

--- src/main/java/org/hidog/main/MainController.java
+++ src/main/java/org/hidog/main/MainController.java
@@ -1,12 +1,35 @@
 package org.hidog.main;
 
 import lombok.RequiredArgsConstructor;
+import org.hidog.board.services.BoardConfigInfoService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/")
 @RequiredArgsConstructor
 public class MainController {
 
+    private final BoardConfigInfoService configInfoService;
+
+    @GetMapping("/main/index2")
+    public String boardList(Model model) {
+
+         List<String[]> boardList = configInfoService.getBoardList();
+
+        model.addAttribute("boardList", boardList);
+
+        model.addAttribute("addCss", List.of("main/style"));
+        model.addAttribute("addScript", List.of( "main/dropdown"));
+
+        model.addAttribute("addCommonCss", List.of("swiper-bundle.min"));
+        model.addAttribute("addScript", List.of( "main/banner"));
+        model.addAttribute("addCommonScript", List.of("swiper-bundle.min"));
+
+        return "front/main/index2";
+    }
 }
