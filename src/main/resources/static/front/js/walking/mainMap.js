68aeec7af77bc8365163da4e9425e6e506a59d2a
KIMJEONGHYEON1016 <dkemfdk4@naver.com>
2024-08-27 11:22:32 +0900

    Feat: 인포창 스타일 추가

--- src/main/resources/static/front/js/walking/mainMap.js
+++ src/main/resources/static/front/js/walking/mainMap.js
@@ -2,4 +2,21 @@ window.addEventListener("DOMContentLoaded", function() {
     if (typeof mainMapLib.load === 'function') {
         mainMapLib.load("mapId");
     }
+
+    const toggleButton = document.getElementById('toggleButton');
+    const infoBox = document.getElementById('infoBox');
+
+    // 초기 상태 설정
+    toggleButton.textContent = '<'; // 닫혀 있을 때
+
+    toggleButton.addEventListener('click', function() {
+        infoBox.classList.toggle('info-box-expanded');
+        if (infoBox.classList.contains('info-box-expanded')) {
+            toggleButton.style.right = '300px';
+            toggleButton.textContent = '>'; // 열렸을 때 화살표 변경
+        } else {
+            toggleButton.style.right = '0px';
+            toggleButton.textContent = '<'; // 닫혔을 때 화살표 변경
+        }
+    });
 })
