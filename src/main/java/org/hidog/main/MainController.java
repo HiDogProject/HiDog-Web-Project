f0de7e9906afb371bece0e5291f940fec5b4e018
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-08-28 15:41:45 +0900

    MainController : 스와이프 부분 수정 필요 + 아이콘 이미지 완성 및 링크 연동 필요 + 서브 배너 이미지 완성 및 글자 추가해야 함

--- src/main/java/org/hidog/main/MainController.java
+++ src/main/java/org/hidog/main/MainController.java
@@ -21,8 +21,16 @@ public class MainController {
     @GetMapping
     public String Main(Model model) {
 
+        List<String[]> boardList = configInfoService.getBoardList();
+
+        model.addAttribute("boardList", boardList);
+
         model.addAttribute("imageUrl", utils.url("/common/img/Hidog.png")); // 로고 이미지
-        model.addAttribute("imageUrl2", utils.url("/common/img/main_dog2.png")); // 메인 이미지
+        model.addAttribute("imageUrl2", utils.url("/common/img/main_dog.png")); // 메인 이미지
+
+        model.addAttribute("addCommonCss", List.of("swiper-bundle.min"));
+        model.addAttribute("addScript", List.of( "main/banner"));
+        model.addAttribute("addCommonScript", List.of("swiper-bundle.min"));
 
         /* 아이콘 이미지 */
         model.addAttribute("imageUrl_D1", utils.url("/common/img/market.gif"));
@@ -33,10 +41,12 @@ public class MainController {
         model.addAttribute("imageUrl_D6", utils.url("/common/img/weather.gif"));
         model.addAttribute("imageUrl_D7", utils.url("/common/img/contest.gif"));
 
-        List<String[]> boardList = configInfoService.getBoardList();
-
         model.addAttribute("addCss", List.of("main/style"));
         model.addAttribute("addScript", List.of( "main/dropdown"));
+
+        /* 서브 배너 이미지 */
+        model.addAttribute("subUrl1", utils.url("/common/img/Jb2.jpg"));
+        model.addAttribute("subUrl2", utils.url("/common/img/Jb1.png"));
 
         return "front/main/index";
     }
